d3104595f7e51499380c21c555c7ae7b5ee6f67a
Edson Barros <edsonabarros01@gmail.com>
2023-03-29 14:16:22 -0300

    Primeira atualização no menu, footer e cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
-import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ContentComponent } from './pages/content/content.component';
@@ -13,26 +12,20 @@ import { FooterComponent } from './components/footer/footer.component';
 import { RightBarComponent } from './components/right-bar/right-bar.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-
 @NgModule({
   declarations: [
     AppComponent,
     BigCardComponent,
     MenuBarComponent,
-    MenuTitleComponent,
     SmallCardComponent,
     HomeComponent,
     ContentComponent,
     FooterComponent,
     RightBarComponent,
     PageNotFoundComponent,
-
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [{ provide: LocationStrategy, useClass: PathLocationStrategy }],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,8 +2,8 @@ export const dataFake = [
   {
     "id":"1",
     "photo": "https://picsum.photos/600",
-    "title": "Veja essa forma de perder peso em 1 semana!",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu rhoncus nisl. Nunc a neque in eros convallis pulvinar sed in tellus. Morbi sed tristique orci. Nullam aliquet malesuada consequat. Mauris elementum nec tortor vitae pharetra. Vivamus elementum ultrices nunc, vel semper mauris. Donec vulputate gravida ex. Phasellus rhoncus odio eros, at lobortis est convallis ut. Quisque euismod placerat urna ac pulvinar. Ut tincidunt mauris et mauris ornare, id malesuada magna luctus. Duis magna mi, mollis in vulputate non, commodo quis nisl. Fusce molestie dignissim ex, ut consequat lectus cursus et."
+    "title": "10 perguntas sobre as últimas notícias da F1 antes do GP da Austrália",
+    "description": "Nesta semana, o circo da Fórmula 1 está chegando ao Albert Park, em Melbourne, para o Grande Prêmio da Austrália – a terceira corrida da temporada de 2023. Mas você tem prestado atenção nas notícias da F1 desde o último Grande Prêmio em Jeddah?"
   },
   {
     "id":"2",
